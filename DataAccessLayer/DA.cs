65f4a827ffdbaffd2550ff0c542fa0a33663f1b1
Artur Ulfig <artur.ulfig2@gmail.com>
2019-08-21 08:31:37 +0200

    DataAccessLayer for Calendar

---
+++ DataAccessLayer/DA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace F1News.DataAccessLayer
+{
+    public class DA
+    {
+        public string _ConnectionStrVC { get; set; }
+
+        public DA(string ConnectionStrVC)
+        {
+            _ConnectionStrVC = ConnectionStrVC;
+        }
+        private SqlConnection GetConnection()
+        {
+            SqlConnection conn = new SqlConnection(_ConnectionStrVC);
+            conn.Open();
+            return conn;
+        }
+        private void CloseConnection(SqlConnection conn)
+        {
+            conn.Close();
+
+        }
+
+    }
+}
